5e2ddf157c944996e600045aae5736992478dd07
Michael Heider <michael.heider@eestec.ethz.ch>
2023-01-22 01:34:02 +0100

    choose random image

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 import os
 import json
 import datetime
+import random
 from collections.abc import Iterable
 from typing import TypedDict
 from dotenv import load_dotenv
@@ -26,11 +27,16 @@ class Node(TypedDict):
     mimeType: str
 
 
+class Folder(TypedDict):
+    id: ID
+    nrFolders: int
+    nrFiles: int
+    nodes: list[Node]
+
+
 class CacheEntry(TypedDict):
     time: str
-    nrOfFolders: int
-    nrOfFiles: int
-    nodes: list[Node]
+    folder: Folder
 
 
 class Main:
@@ -45,14 +51,12 @@ class Main:
 
     MIME_TYPE_FOLDER: str = 'application/vnd.google-apps.folder'
 
-    def readEnv(self) -> dict:
+    def readEnv(self) -> dict[str, any]:
         load_dotenv()
         self.env = {
             'DRIVE_ID': os.getenv('DRIVE_ID'),
             'ROOT_FOLDER_ID': os.getenv('ROOT_FOLDER_ID'),
-            # make sure to not check this into Git
             'CREDENTIALS_FILE': os.getenv('CREDENTIALS_FILE'),
-            # make sure to not check this into Git
             'TOKEN_FILE': os.getenv('TOKEN_FILE', 'token.json'),
             'CACHE_FILE': os.getenv('CACHE_FILE', 'cache.json'),
         }
@@ -92,7 +96,7 @@ class Main:
         with open(self.env['CACHE_FILE'], 'w') as f:
             json.dump(self.cache, f, indent=4, check_circular=False)
 
-    def googleListFiles(self, folderId: str, queryParams: dict[any] = dict()) -> list[Node]:
+    def googleListNodes(self, folderId: str, queryParams: dict[any] = dict()) -> list[Node]:
         files = []
         pageToken = None
         # iterate over pages
@@ -114,17 +118,13 @@ class Main:
 
         return files
 
-    def getFolderContent(self, folderId: str, files: bool = True, folders: bool = True, forceUpdate: bool = False) -> list[Node] | Iterable[Node]:
+    def getFolder(self, folderId: ID, forceUpdate: bool = False) -> Folder:
         """
-        Retrieve files and/or folders from given folder.
-
-        Implementation note: This always gets the whole folder content from Google,
-        but caches the result. This simplifies implementation, since we can just
-        filter for files or folders after either hitting cache or querying Google.
+        Implementation note:
+        If the cache misses, this gets the whole folder content from Google and
+        caches it. This allows us to compute folder statistics.
 
         @param folderId: Google folder ID.
-        @param files: Include files.
-        @param folders: Include folders.
         @param forceUpdate: Force update cache.
         """
         # Standard params for Google Drive API file list.
@@ -137,55 +137,112 @@ class Main:
             'driveId': self.env['DRIVE_ID']
         }
 
-        nodes = []
+        folder = None
         # check cache
         item = self.cache.get(folderId, None)
         # no force update, no miss, not stale
         if not forceUpdate and item is not None and datetime.datetime.utcnow() - datetime.datetime.fromisoformat(item['time']) < datetime.timedelta(days=30):
             # cache hit
             print("  cache: hit")
-            nodes = self.cache[folderId]['nodes']
+            return self.cache[folderId]['folder']
         else:
             # cache miss, stale value or forced update
             print("  cache: query Google")
-            nodes = self.googleListFiles(folderId, STANDARD_PARAMS)
-            self.cache[folderId] = {
-                'time': datetime.datetime.utcnow().isoformat(timespec='seconds'),
-                'nrOfFolders': sum(1 for node in nodes if node['mimeType'] == self.MIME_TYPE_FOLDER),
-                'nrOfFiles': sum(1 for node in nodes if node['mimeType'] != self.MIME_TYPE_FOLDER),
-                'nodes': nodes,
-            }
+            nodes = self.googleListNodes(folderId, STANDARD_PARAMS)
+            folder = Folder(
+                id=folderId,
+                nrFolders=sum(
+                    1 for node in nodes if node['mimeType'] == self.MIME_TYPE_FOLDER),
+                nrFiles=sum(
+                    1 for node in nodes if node['mimeType'] != self.MIME_TYPE_FOLDER),
+                nodes=nodes
+            )
+            self.cache[folderId] = CacheEntry(
+                time=datetime.datetime.utcnow().isoformat(timespec='seconds'),
+                folder=folder
+            )
             self.writeBackCache()
+            return folder
+
+    def getFolderContent(self, folderId: ID, files: bool = True, folders: bool = True, forceUpdate: bool = False) -> list[Node]:
+        """
+        Returns files and/or folders from given folder.
+
+        @param folderId: Google folder ID.
+        @param files: Include files.
+        @param folders: Include folders.
+        @param forceUpdate: Force update cache.
+        """
+        nodes = self.getFolder(folderId, forceUpdate)['nodes']
 
         # filter result
         if folders and not files:
-            return filter(lambda node: node['mimeType'] == self.MIME_TYPE_FOLDER, nodes)
+            return [node for node in nodes if node['mimeType'] == self.MIME_TYPE_FOLDER]
+            # return filter(lambda node: node['mimeType'] == self.MIME_TYPE_FOLDER, nodes)
         elif files and not folders:
-            return filter(lambda node: node['mimeType'] != self.MIME_TYPE_FOLDER, nodes)
+            return [node for node in nodes if node['mimeType'] != self.MIME_TYPE_FOLDER]
+            # return filter(lambda node: node['mimeType'] != self.MIME_TYPE_FOLDER, nodes)
         elif files and folders:
             return nodes
         else:
             # Programmer fucked up.
             raise ValueError("Cannot return neither files nor folders.")
-    
-    def recursivePictureFinding(self, folderId: ID):
-        folders = self.getFolderContent(folderId, False, True)
-        for folder in folders:
-            self.recursivePictureFinding(folder)
 
-    def run(self):
-        print("query top level folder")
+    # def getFolder(self, folderId: str, forceUpdate: bool = False) -> Folder:
+    #     """ Get a folder. Ensures that statistics in node exist. """
+    #     # make sure cache entry exists
+    #     self.getFolderContent(folderId, True, True, forceUpdate)
+    #     return self.cache[folderId]['folder']
+
+    # def recursivePictureFinding(self, folderId: ID):
+    #     folders = self.getFolderContent(folderId, False, True)
+    #     for folder in folders:
+    #         self.recursivePictureFinding(folder)
+    #     files = self.getFolderContent(folderId, True, False)
+    #     pictureDB[folderId] = files
+
+    def chooseRandomPictureRecursive(self, folder: Folder) -> Node:
+        folderId = folder['id']
+        hasFiles = folder['nrFiles'] > 0
+        n = folder['nrFolders']
+        if hasFiles > 0:
+            n += 1
+        rFolder = random.randint(0, n-1)
+        if hasFiles and rFolder == n-1:
+            # pick file from current folder
+            rFile = random.randint(0, folder['nrFiles']-1)
+            return self.getFolderContent(folderId, True, False)[rFile]
+        else:
+            # descend one layer
+            nextNode = self.getFolderContent(folderId, False, True)[rFolder]
+            nextFolder = self.getFolder(nextNode['id'])
+            return self.chooseRandomPictureRecursive(nextFolder)
+
+    def chooseRandomPicture(self) -> Node:
+        topLevelFolder = self.getFolder(self.env['ROOT_FOLDER_ID'])
+        nrFolders = topLevelFolder['nrFolders']
         topLevelFolders = self.getFolderContent(
             self.env['ROOT_FOLDER_ID'], False, True)
+        r = random.randint(0, nrFolders-1)
+        nextFolder = self.getFolder(topLevelFolders[r]['id'])
+        return self.chooseRandomPictureRecursive(nextFolder)
+
+    def run(self):
+        # topLevelFolders = self.getFolderContent(
+        #     self.env['ROOT_FOLDER_ID'], False, True)
+
         # for folder in topLevelFolders:
         #     print('id: {0}, name: {1}, mimeType: {2}'.format(
         #         folder['id'], folder['name'], folder['mimeType']))
         # print(len(topLevelFolders))
 
-        for folder in topLevelFolders:
-            print(f"query folder: '{folder['name']}'")
-            folderId = folder['id']
-            folders = self.getFolderContent(folderId, True, True)
+        # for folder in topLevelFolders:
+        #     print(f"query folder: '{folder['name']}'")
+        #     folderId = folder['id']
+        #     folders = self.getFolderContent(folderId, True, True)
+
+        picture = self.chooseRandomPicture()
+        print(picture)
 
     def __init__(self):
         self.readEnv()
@@ -202,7 +259,7 @@ class Main:
                     self.cache = json.load(f)
                 except json.decoder.JSONDecodeError as error:
                     # cache file invalid
-                    print('Cache file invalid. Recreate.')
+                    print('Cache file invalid. Delete.')
                     with open(self.env['CACHE_FILE'], 'w') as f:
                         f.truncate(0)
         else:
