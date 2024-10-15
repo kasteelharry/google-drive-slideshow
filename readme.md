6f9bc39c53f21803032b981aeba21db0d6d0ea82
Joris Kuiper <joriskuiper2@gmail.com>
2024-10-15 11:13:13 +0200

    Add initial video support and distribution based on folder sizes

--- readme.md
+++ readme.md
@@ -8,6 +8,10 @@ This does work with shared Drives.
 
 Starting with the root folder, recursively select a random folder (or itself, if it contains at least one file). Once we hit a folder with only files, pick a random file. Validate it to be of supported image type and not respect the `MAX_FILE_SIZE` parameter. If we ever run into an issue (unsopported file, empty folder, etc.) try again from the root.
 
+This fork also added initial support for videos. However, there are lot of issues with the framerate and fps of these videos resulting in some slowed down or sped up videos. Furthermore, sound is not supported.
+
+Another addition made in this fork is that all files in the folders in the root folder will summed up and put in a dictionary. This dictionary will then be used to build a distribution which is further used to select images such that folders with more images will be selected more often. The downside of this will be that the first run of this script will take at least 5 minutes to initialize the distribution.
+
 ## Setup
 
 You need to create a Google Cloud project, enable the API `drive.readonly`, Configure OAuth, and create access credentials. Make sure the Google account you are using has access to the files you want for the slideshow.
@@ -24,6 +28,7 @@ Furthermore, setup a `.env` file containing at least the following parameters:
 Optional parameters:
 
 - `MAX_FILE_SIZE`: Maximum allowable file size in MB. Larger files are skipped.
+- `MAX_VIDEO_LENGTH`: Maximum length of the videos. Longer videos are skipped.
 - `PICTURE_KEEP_NR`: How many pictures are kept before they are deleted again. This can be useful, if you want to have another look at a past but recent picture.
 
 There are a few more technical options, which you can find in the `Slideshow` class in the `__readEnv` method. (for advanced users)
@@ -38,6 +43,8 @@ You need Python 3 and pip.
 
 Install dependencies from `requirements.txt` with `pip install -r requirements.txt`.
 
+Finally, TKinter needs to be installed as well, see Developer section
+
 ## License
 
 This software is licensed under GPL 3.0 or later, see license file.
@@ -46,7 +53,9 @@ This software is licensed under GPL 3.0 or later, see license file.
 
 This project was created to show off pictures of [EESTEC LC Zurich](https://eestec.ethz.ch) by Michael Heider in January 2023, then Chairman of EESTEC LC Zurich.
 
-## Developper
+This fork was made to show off pictures of [D.S.T.V. Aloha](https://www.alohatriathlon.nl/) during their 6th lustrum in Oktober 2024.
+
+## Developer
 
 Help for the developer of this software and his random notes.
 
--- slideshow.py
+++ slideshow.py
@@ -6,10 +6,11 @@ import random
 import collections
 import shutil
 import json
+import cv2
 from colorama import Fore, Back, Style
 from dotenv import load_dotenv
 import tkinter as tk
-from PIL import Image, ImageTk, UnidentifiedImageError
+from PIL import Image, ImageTk, UnidentifiedImageError, ImageFile
 from pillow_heif import register_heif_opener
 from googleapiclient.errors import HttpError
 from fileSystem import FileSystem, Folder, File
@@ -30,15 +31,70 @@ class Slideshow:
     __WIDTH_DISPLAY_HALF: float
     __HEIGHT_DISPLAY_HALF: float
 
+    __photoDistribution: dict
+    __sum: int
+
     SUPPORTED_IMAGE_MIME_TYPES = [
         'image/jpeg',
         'image/png',
         'image/heif',
         'image/x-photoshop',
         'image/cr2',
+        'video/mp4',
+        'video/mpeg',
+        'video/quicktime',
+        'video/x-ms-wmv',
+        'video/x-msvideo',
     ]
 
+    VIDEO_TYPES = [
+        'video/mp4',
+        'video/mpeg',
+        'video/quicktime',
+        'video/x-msvideo',
+    ]
+
+
     class __DirectoryEmptyException(Exception):
+        pass
+
+    def __createPhotoDistribution(self) -> dict:
+        '''
+        Builds a dictionary of every top level folder in the root folder
+        This distribution is then used to select a photo or video where
+        folders with more files is selected more often.
+        '''
+        dist = dict()
+        graph = dict()
+        for i in range(0, self.__rootFolder['nrFolders']):
+            nextNode = self.__fileSystem.filterNodes(
+                self.__rootFolder['nodes'], True, False)[i]
+            nextFolder = self.__fileSystem.getFolder(nextNode)
+            dist[nextFolder['id']] = self.__getPhotoCount(nextFolder)
+            graph[nextFolder['name']] = dist[nextFolder['id']]
+        print(graph)
+        return dict(sorted(dist.items(), key=lambda item: item[1]))
+
+    def __getPhotoCount(self, folder: Folder) -> int:
+        if (folder['nrFolders'] <= 0):
+            return folder['nrFiles']
+        sum = folder['nrFiles']
+        for i in range(0, folder['nrFolders']):
+            nextNode = self.__fileSystem.filterNodes(
+                folder['nodes'], True, False)[i]
+            nextFolder = self.__fileSystem.getFolder(nextNode)
+            sum += self.__getPhotoCount(nextFolder)
+        return sum
+    
+    def __getRandomPhotoDict(self) -> tuple[str, int]:
+        # Generate a random number between 0 and total_sum
+        random_number = random.randint(0, self.__sum - 1)
+        # Iterate through the dictionary and find where the random number lies
+        cumulative_sum = 0
+        for key, value in self.__photoDistribution.items():
+            cumulative_sum += value
+            if random_number < cumulative_sum:
+                return (key, random_number)
         pass
 
     def __readEnv(self) -> None:
@@ -57,6 +113,8 @@ class Slideshow:
             'PICTURE_KEEP_NR': int(os.getenv('PICTURE_KEEP_NR', 10)),
             # MAX_FILE_SIZE in MB, -1 to disable
             'MAX_FILE_SIZE': int(os.getenv('MAX_FILE_SIZE', -1))*1_000_000,
+            # MAX_VIDEO_LENGTH in minutes, -1 to disable all videos
+            'MAX_VIDEO_LENGTH': int(os.getenv('MAX_VIDEO_LENGTH', -1)) * 60
         }
 
         # validate tempFolder
@@ -77,13 +135,15 @@ class Slideshow:
                 'Environment variables are invalid. Check your `.env`.')
 
     def __chooseRandomFileRec(self, folder: Folder) -> tuple[File, str]:
-        hasFiles = folder['nrFiles'] > 0
         n = folder['nrFolders']
-        if hasFiles > 0:
-            n += 1
         if n == 0:
             raise self.__DirectoryEmptyException(
                 "Directory '{0}' is empty.".format(folder['id']))
+
+        hasFiles = folder['nrFiles'] > 0
+        if hasFiles > 0:
+            n += 1
+        
         rFolder = random.randint(0, n-1)
         if hasFiles and rFolder == n-1:
             # pick file from current folder
@@ -100,13 +160,13 @@ class Slideshow:
             return file, nextFolder['name'] + "/" + path
 
     def __chooseRandomFileFirstLevel(self) -> tuple[File, str]:
-        # TODO: bias this towards newer folders
         topLevelFolder = self.__fileSystem.getFolder(self.__rootFolder)
-        nrFolders = topLevelFolder['nrFolders']
         topLevelFolders = self.__fileSystem.filterNodes(
             topLevelFolder['nodes'], True, False)
-        r = random.randint(0, nrFolders-1)
-        nextFolder = self.__fileSystem.getFolder(topLevelFolders[r])
+        # Show folders with more pictures more often
+        r = self.__getRandomPhotoDict()
+        nextFolderObject = next(filter(lambda obj: obj['id'] == r[0], topLevelFolders), None)
+        nextFolder = self.__fileSystem.getFolder(nextFolderObject)
         file, path = self.__chooseRandomFileRec(nextFolder)
         return file, nextFolder['name'] + "/" + path
 
@@ -128,7 +188,7 @@ class Slideshow:
                     else:
                         print(f"choose: file too large, retrying ('{path}')")
                 else:
-                    print(f"choose: unsupported file type, retrying ('{path}')")
+                    print(f"choose: unsupported file type, retrying ('{path}', '{file['mimeType']}')")
             except self.__DirectoryEmptyException:
                 # try again, rejection sampling
                 print('choose: empty directory, retrying')
@@ -148,25 +208,7 @@ class Slideshow:
             }, check_circular=False))
             f.write(f',{os.linesep}')
 
-    def __display_next_slide(self) -> None:
-        print('Get next slide')
-        file, path, pathLocal = self.__getRandomPicture()
-        print(f"Got next slide: '{path}'")
-
-        self.__log.append(file)
-        if len(self.__log) == self.__log.maxlen:
-            oldFile = self.__log.popleft()
-            self.__fileSystem.deleteFile(oldFile)
-        self.__logToFile(file, path)
-
-        try:
-            pilImage: Image = Image.open(pathLocal)
-        except (UnidentifiedImageError, OSError):
-            # image is unsupported or corrupted
-            # try again
-            self.__slideshow.after(0, self.__display_next_slide)
-            return
-
+    def __resize(self, pilImage):
         # resize image to full screen size
         imgWidth, imgHeight = pilImage.size
         # The .load() call is not necessary, but a workaround for
@@ -178,18 +220,61 @@ class Slideshow:
                     self.__HEIGHT_DISPLAY_HALF/imgHeight)
         imgWidthFull = int(imgWidth*ratio)
         imgHeightFull = int(imgHeight*ratio)
-        pilImage = pilImage.resize(
-            (imgWidthFull, imgHeightFull), Image.ANTIALIAS)
+        return pilImage.resize(
+            (imgWidthFull, imgHeightFull), Image.LANCZOS)
 
+
+    def __display_next_slide(self) -> None:
+        print('Get next slide')
+        file, path, pathLocal = self.__getRandomPicture()
+        print(f"Got next slide: '{path}'")
+
+        self.__log.append(file)
+        if len(self.__log) == self.__log.maxlen:
+            oldFile = self.__log.popleft()
+            self.__fileSystem.deleteFile(oldFile)
+        self.__logToFile(file, path)
+        
+        self.__slideshow.title(path)
+
+
+        if file['mimeType'] in self.VIDEO_TYPES:
+            self.__currentSlide.delete('all')
+        
+            video = cv2.VideoCapture(pathLocal)
+            fps = video.get(cv2.CAP_PROP_FPS)
+            frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+            duration = frame_count/fps
+            max_duration = self.__env['MAX_VIDEO_LENGTH']
+
+            if duration > max_duration:
+                print(f'Video length was too long, expected {max_duration}, received {duration}')
+                self.__slideshow.after(0, self.__display_next_slide)
+                return
+            self.__displayVideo(video, fps)
+            return
+        try:
+            pilImage: Image = Image.open(pathLocal)
+        except (UnidentifiedImageError, OSError):
+            # image is unsupported or corrupted
+            # try again
+            self.__slideshow.after(0, self.__display_next_slide)
+            return
+
+        pilImage = self.__resize(pilImage)
         # need to store iamge to not have it garbage collected immediately
         self.__nextImage = ImageTk.PhotoImage(pilImage)
-
+        
         self.__currentSlide.create_image(
-            self.__WIDTH_DISPLAY_HALF/2, self.__HEIGHT_DISPLAY_HALF/2, image=self.__nextImage)
-        self.__slideshow.title(path)
+            self.__WIDTH_DISPLAY_HALF/2, self.__HEIGHT_DISPLAY_HALF/2, image=self.__nextImage, )
+
+        text_string = str(path).split('/')[:-1]
+        text = self.__currentSlide.create_text(self.__WIDTH_DISPLAY_HALF/2, 50, text=" ".join(text_string), fill="orange", font=('Helvetica 25 bold'))
 
         self.__slideshow.after(
             self.__env['SLIDESHOW_SPEED'], self.__display_next_slide)
+        self.__slideshow.after(
+            self.__env['SLIDESHOW_SPEED'], self.__currentSlide.delete, text)
 
     def __onWindowResize(self, event) -> None:
         """ Adapt values such that the next rendered image is again maximum size. """
@@ -199,6 +284,51 @@ class Slideshow:
             self.__currentSlide.configure(
                 width=self.__WIDTH_DISPLAY_HALF, height=self.__HEIGHT_DISPLAY_HALF)
 
+    def __resize_frame(self, frame, target_width, target_height):
+        original_height, original_width = frame.shape[:2]
+
+        # Calculate the scaling factor
+        scaling_factor = min(target_width / original_width, target_height / original_height)
+
+        # Resize the frame while preserving aspect ratio
+        new_width = int(original_width * scaling_factor)
+        new_height = int(original_height * scaling_factor)
+        
+        # Resize the frame using OpenCV
+        resized_frame = cv2.resize(frame, (new_width, new_height))
+        return resized_frame
+
+
+    def __displayVideo(self, video, fps) -> None:
+        ret, frame = video.read()
+
+        if not ret:
+            video.release()
+            self.__currentSlide.delete('all')
+            self.__currentSlide.after(1, self.__display_next_slide) 
+            return
+        frame = self.__resize_frame(frame, 1920, 1080)
+        cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        img = Image.fromarray(cv2image)
+
+        imgtk = ImageTk.PhotoImage(img)
+        self.__currentSlide.imgtk = imgtk
+        self.__currentSlide.create_image(
+            self.__WIDTH_DISPLAY_HALF/2, self.__HEIGHT_DISPLAY_HALF/2, image=imgtk, )
+        self.__currentSlide.configure(background='black')
+
+        if fps > 0:
+            frame_delay_ms = int(500 / fps)
+        else:
+            frame_delay_ms = 33 
+        self.__currentSlide.after(frame_delay_ms, self.__displayVideo, video, fps) 
+        pass
+
+    def __toggle_fullscreen(self, event=None):
+        # Toggle fullscreen mode
+        current = self.__slideshow.attributes("-fullscreen")
+        self.__slideshow.attributes("-fullscreen", 1 - current)
+
     def run(self) -> None:
         self.__display_next_slide()
         self.__slideshow.mainloop()
@@ -206,8 +336,10 @@ class Slideshow:
         # cleanup
         # temp folder is not cleaned, in case we want to check one of the recent pictures.
         print('User closed window. Slideshow terminated.')
+        exit(0)
 
     def __init__(self) -> None:
+        ImageFile.LOAD_TRUNCATED_IMAGES = True
         self.__readEnv()
         register_heif_opener()
         self.__fileSystem = FileSystem(self.__env)
@@ -216,6 +348,11 @@ class Slideshow:
         self.__rootFolder = self.__fileSystem.getFolder(
             Folder(id=self.__env['ROOT_FOLDER_ID'], name="", nrFolders=-1, nrFiles=-1, nodes=[]))
         self.__log = collections.deque(maxlen=self.__env['PICTURE_KEEP_NR'])
+
+        self.__photoDistribution = self.__createPhotoDistribution()
+        # Calculate the total sum of the valid values
+        self.__sum = sum(self.__photoDistribution.values())
+
 
         # force initialize cache
         # self.__fileSystem.forceInitialize(self.__rootFolder)
@@ -231,8 +368,8 @@ class Slideshow:
         self.__WIDTH_DISPLAY_HALF = self.__slideshow.winfo_screenwidth()
         self.__HEIGHT_DISPLAY_HALF = self.__slideshow.winfo_screenheight()
         # Override window size for testing
-        self.__WIDTH_DISPLAY_HALF = 300
-        self.__HEIGHT_DISPLAY_HALF = 300
+        # self.__WIDTH_DISPLAY_HALF = 300
+        # self.__HEIGHT_DISPLAY_HALF = 300
 
         self.__slideshow.title("Slideshow")
         self.__slideshow.geometry(
@@ -240,25 +377,13 @@ class Slideshow:
         self.__slideshow.resizable(width=True, height=True)
         self.__slideshow.bind("<Configure>", self.__onWindowResize)
         self.__slideshow.bind("<Escape>", lambda e: e.widget.quit())
+        self.__slideshow.bind("<F11>", self.__toggle_fullscreen)
         self.__slideshow.focus_set()
 
         self.__currentSlide = tk.Canvas(
             self.__slideshow, width=self.__WIDTH_DISPLAY_HALF, height=self.__HEIGHT_DISPLAY_HALF)
         self.__currentSlide.pack()
         self.__currentSlide.configure(background='black')
-
-        # force overlay over everything in fullscreen
-        # unfortunately, this also blocks closing why key press
-        # may not work on other platforms than Linux (?)
-        # self.slideshow.overrideredirect(1)
-
-        # sanity check Google Drive API
-        # topLevelFolder = self.__fileSystem.getFolder(self.__rootFolder)
-        # print("sanity check Google Drive API")
-        # print("top level name:      '{0}'".format(topLevelFolder['name']))
-        # print("top level subfolders: {0:>3}".format(
-        #     topLevelFolder['nrFolders']))
-        # print("top level files:      {0:>3}".format(topLevelFolder['nrFiles']))
 
 
 if __name__ == '__main__':
